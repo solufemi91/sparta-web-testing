c84107699494ac7b5989b0a39d14b3075a035613
solufemi91 <stevenoo@spartaglobal.com>
2017-12-20 22:09:53 +0000

    removed sleep from a spec file

--- capybara/capybara_rspec.rb/spec/bbc_login_error_spec.rb
+++ capybara/capybara_rspec.rb/spec/bbc_login_error_spec.rb
@@ -11,7 +11,6 @@ describe 'Incorrect user details produces valid error' do
     it 'should produce an error when inputting an incorrect password to a valid account' do
       @bbc_site = BbcSite.new
       @bbc_site.bbc_homepage.visit_home_page
-      # sleep 4
       @bbc_site.bbc_homepage.click_sign_in_link
       @bbc_site.bbc_sign_in_page.fill_in_username('solufemi-olukanni@hotmail.com')
       @bbc_site.bbc_sign_in_page.fill_in_password('doghduidiuhdue')
